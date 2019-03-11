d47b0d5ef086da1fc6e6811a107e138c97894cc5
Jason  Montes <jasone.montes@gmail.com>
2019-03-11 05:08:48 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
-def addition(num1, num2)
+def addition(num1, num2) 
+  4+5 = 9
 end
 
 def subtraction(num1, num2)
+  10-5 = 5
 end
 
 def division(num1, num2)
+  50/2 = 25
 end
 
 def multiplication(num1, num2)
+  4*30 = 120
 end
 
 def modulo(num1, num2)
+  34%5 = 4
 end
 
 def square_root(num)
+  Math.sqrt(81) = 9
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  7+((43*23)/83) = 18
 end
